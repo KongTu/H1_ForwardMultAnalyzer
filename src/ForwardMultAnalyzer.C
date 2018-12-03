44c3caeca6d429ba380fa640eb46680aa57068e6
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-12-03 17:03:49 -0500

    update to get trigger and weights from Stefan

--- src/ForwardMultAnalyzer.C
+++ src/ForwardMultAnalyzer.C
@@ -224,6 +224,28 @@ int main(int argc, char* argv[]) {
    H1StdCmdLine opts;
    opts.Parse(&argc, argv);
 
+ 
+   // // open run selection and detector status file
+   // TString goodRunFileName("SelectedRuns_0607-and-lowE.root");
+   // TFile goodRunFile(goodRunFileName);
+   // if(!goodRunFile.IsOpen()) {
+   //    cerr<<"Error: could not open file "<<goodRunFileName<<"\n";
+   //    return 2;
+   // }
+   // H1RunList* goodRunList
+   //    = (H1RunList*) goodRunFile.Get("H1RunList");
+   // if(!goodRunList) {
+   //    cerr<<"Error: no runlist in file - return!\n";
+   //    return 2;
+   // }
+   // H1DetectorStatus *detectorStatus
+   //    = (H1DetectorStatus*)goodRunFile.Get("MyDetectorStatus");
+   // if(!detectorStatus) {
+   //    cerr<<"Error: no detector status in file - return!\n";
+   //    return 3;
+   // }
+   
+
    // Load mODS/HAT files
    H1Tree::Instance()->Open();            // this statement must be there!
 
@@ -384,38 +406,16 @@ int main(int argc, char* argv[]) {
    hadronicCalibration->ApplyHadronicCalibration(H1HadronicCalibration::eHighPtJet);
    hadronicCalibration->ApplyHadronicCalibration(kTRUE);
 
-   // open run selection and detector status file
-   TString goodRunFileName("SelectedRuns_0607-and-lowE.root");
-   TFile goodRunFile(goodRunFileName);
-   if(!goodRunFile.IsOpen()) {
-      cerr<<"Error: could not open file "<<goodRunFileName<<"\n";
-      if(*runtype!=1) return 2;
-   }
-   H1RunList* goodRunList
-      = (H1RunList*) goodRunFile.Get("H1RunList");
-   if(!goodRunList) {
-      cerr<<"Error: no runlist in file - return!\n";
-       if(*runtype!=1) return 2;
-   }
-   H1DetectorStatus *detectorStatus
-      = (H1DetectorStatus*)goodRunFile.Get("MyDetectorStatus");
-   if(!detectorStatus) {
-      cerr<<"Error: no detector status in file - return!\n";
-      if(*runtype!=1) return 3;
-   }
-   
-
    // Loop over events
    static int print=10;
    while (gH1Tree->Next() && !opts.IsMaxEvent(eventCounter)) {
       ++eventCounter;
 
-      if(*runtype!=1){
-         // skip runs not in list of good runs
-         if(!goodRunList->FindRun(*run)) continue;
-         // skip data events with bad detector status
-         if(!detectorStatus->IsOn()) continue;
-      }
+      //    // skip runs not in list of good runs
+      //    if(!goodRunList->FindRun(*run)) continue;
+      //    // skip data events with bad detector status
+      //    if(!detectorStatus->IsOn()) continue;
+      
       double w=*weight1 * *weight2;
       // if(*Q2Gki<10.) continue;
       if(print || ((eventCounter %10000)==0))  { 
