7e2e373bfd0561792411a50a3d8124f0e7770311
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-02-28 13:03:31 -0500

    add lee west cut variables

--- src/ForwardMultAnalyzer.C
+++ src/ForwardMultAnalyzer.C
@@ -968,16 +968,17 @@ int main(int argc, char* argv[]) {
 
                      }
 
-                     // H1CombinedFittedTrack const *combtrack = 0;
-                     // if(combtrack){
-                        chi2Link=h1track->GetLinkChi2();
-                        ndfLink=h1track->GetLinkNdf();
-                     //}
+                     H1CombinedFittedTrack const *combtrack = 
+                        h1track->GetCombinedFittedTrack();  
+                     if(combtrack){
+                        chi2Link=combtrack->GetLinkChi2();
+                        ndfLink=combtrack->GetLinkNdf();
+                     }
                      
-                     //H1ForwardFittedTrack const *fwdtrack = 0;
-                     //if(fwdtrack){
-                        rZero = h1track->GetR0();
-                     //}
+                     H1ForwardFittedTrack const *fwdtrack = 0;
+                     if(fwdtrack){
+                        rZero = fwdtrack->GetR0();
+                     }
 
                      H1Vertex const *v=h1track->GetVertex();
                      if(floatEqual(v->X(),myEvent.vertex[0])&&
