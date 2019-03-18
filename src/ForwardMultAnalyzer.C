b7f94a163f1593f59701e8825b23805d7d960f97
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-03-18 17:25:52 +0100

    start to test radiative MC

--- src/ForwardMultAnalyzer.C
+++ src/ForwardMultAnalyzer.C
@@ -509,25 +509,25 @@ int main(int argc, char* argv[]) {
             (mcpart[mcPartId.GetIdxScatElectron()]->GetFourVector());
 
          /*begin test*/
-            cout << "test " << mcPartId.GetIdxRadPhoton() << endl;
-         // if( mcPartId.GetIdxRadPhoton() != 0 ){
-         //    TLorentzVector radPhot_MC_lab
-         //    (mcpart[mcPartId.GetIdxRadPhoton()]->GetFourVector());
+         if( mcPartId.GetIdxRadPhoton() >= 0 ){
+            TLorentzVector radPhot_MC_lab
+            (mcpart[mcPartId.GetIdxRadPhoton()]->GetFourVector());
             
-         //    double delta_phi = escat0_MC_lab.Phi() - radPhot_MC_lab.Phi();
-         // }
-            // if( mcPartId.GetRadType() == 0 ){
-            //    h_dPhi_theta_noR->Fill(escat0_MC_lab.Theta(), delta_phi );
-            // }
-            // else if( mcPartId.GetRadType() == 1 ){
-            //    h_dPhi_theta_ISR->Fill(escat0_MC_lab.Theta(), delta_phi );
-            // }
-            // else if( mcPartId.GetRadType() == 2 ){
-            //    h_dPhi_theta_FSR->Fill(escat0_MC_lab.Theta(), delta_phi );
-            // }
-            // else{
-            //    cout << "something is wrong!" << endl;
-            // }
+               double delta_phi = escat0_MC_lab.Phi() - radPhot_MC_lab.Phi();
+         
+            if( mcPartId.GetRadType() == 0 ){
+               h_dPhi_theta_noR->Fill(escat0_MC_lab.Theta(), delta_phi );
+            }
+            else if( mcPartId.GetRadType() == 1 ){
+               h_dPhi_theta_ISR->Fill(escat0_MC_lab.Theta(), delta_phi );
+            }
+            else if( mcPartId.GetRadType() == 2 ){
+               h_dPhi_theta_FSR->Fill(escat0_MC_lab.Theta(), delta_phi );
+            }
+            else{
+               cout << "something is wrong!" << endl;
+            }
+         }
          //end test
 
          // add radiative photon(s) in a cone
