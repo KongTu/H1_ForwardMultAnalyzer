b3bbce6002a2ad250bfde4367c579046021dede5
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-03-18 18:18:29 +0100

    start to test radiative MC

--- src/ForwardMultAnalyzer.C
+++ src/ForwardMultAnalyzer.C
@@ -549,11 +549,14 @@ int main(int argc, char* argv[]) {
          double hfs_MC_pz_lab = hfs_MC_lab.Pz();
          double sigma = hfs_MC_E_lab - hfs_MC_pz_lab;
 
-         H1MakeKine* makeKin = 0;
+         H1MakeKine* makeKin;
          makeKin->MakeESig(escat0_MC_lab.E(), escat0_MC_lab.Theta(), sigma, ebeam_MC_lab.E(), pbeam_MC_lab.E());
-         double Q2_esigma = makeKin->GetQ2es();
-         double y_esigma = makeKin->GetYes();
-         double x_esigma = makeKin->GetXes();
+         double Q2_esigma = 0.;
+         double y_esigma = 0.;
+         double x_esigma = 0.;
+         // double Q2_esigma = makeKin->GetQ2es();
+         // double y_esigma = makeKin->GetYes();
+         // double x_esigma = makeKin->GetXes();
          //end test
 
          // add radiative photon(s) in a cone
