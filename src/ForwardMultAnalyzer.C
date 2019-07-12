9a8978abb21106482bbabe8c8293fd32d5e0873c
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-07-12 14:48:20 -0400

    add +2% HFS energy scale

--- src/ForwardMultAnalyzer.C
+++ src/ForwardMultAnalyzer.C
@@ -306,24 +306,24 @@ int main(int argc, char* argv[]) {
    opts.Parse(&argc, argv);
 
    // open run selection and detector status file
-   // TString goodRunFileName("SelectedRuns_HighE0607_BST_e+p_920.root");
-   // TFile goodRunFile(goodRunFileName);
-   // if(!goodRunFile.IsOpen()) {
-   //    cerr<<"Error: could not open file "<<goodRunFileName<<"\n";
-   //    return 2;
-   // }
-   // H1RunList* goodRunList
-   //    = (H1RunList*) goodRunFile.Get("H1RunList");
-   // if(!goodRunList) {
-   //    cerr<<"Error: no runlist in file - return!\n";
-   //    return 2;
-   // }
-   // H1DetectorStatus *detectorStatus
-   //    = (H1DetectorStatus*)goodRunFile.Get("MyDetectorStatus");
-   // if(!detectorStatus) {
-   //    cerr<<"Error: no detector status in file - return!\n";
-   //    return 3;
-   // }
+   TString goodRunFileName("SelectedRuns_HighE0607_BST_e+p_920.root");
+   TFile goodRunFile(goodRunFileName);
+   if(!goodRunFile.IsOpen()) {
+      cerr<<"Error: could not open file "<<goodRunFileName<<"\n";
+      return 2;
+   }
+   H1RunList* goodRunList
+      = (H1RunList*) goodRunFile.Get("H1RunList");
+   if(!goodRunList) {
+      cerr<<"Error: no runlist in file - return!\n";
+      return 2;
+   }
+   H1DetectorStatus *detectorStatus
+      = (H1DetectorStatus*)goodRunFile.Get("MyDetectorStatus");
+   if(!detectorStatus) {
+      cerr<<"Error: no detector status in file - return!\n";
+      return 3;
+   }
 
    // Load mODS/HAT files
    H1Tree::Instance()->Open();            // this statement must be there!
@@ -540,10 +540,10 @@ int main(int argc, char* argv[]) {
    while (gH1Tree->Next() && !opts.IsMaxEvent(eventCounter)) {
       ++eventCounter;
 
-         // // skip runs not in list of good runs
-         // if(!goodRunList->FindRun(*run)) continue;
-         // // skip data events with bad detector status
-         // if(!detectorStatus->IsOn()) continue;
+         // skip runs not in list of good runs
+         if(!goodRunList->FindRun(*run)) continue;
+         // skip data events with bad detector status
+         if(!detectorStatus->IsOn()) continue;
 
       double w=*weight1 * *weight2;
       if(print || ((eventCounter %10000)==0))  { 
@@ -1096,6 +1096,12 @@ int main(int argc, char* argv[]) {
             hfs_count += p;
          }
       }
+
+      double hfs_eta = hfs_count.Eta();
+      double hfs_phi = hfs_count.Phi();
+      double hfs_pt = hfs_count.Pt();
+      double hfs_E = 1.02*hfs_count.E();
+      hfs_count.SetPtEtaPhiE(hfs_pt,hfs_eta,hfs_phi,hfs_E);
 
       double sigma_REC = hfs_count.E()-hfs_count.Pz();//not use for Elec method
       
