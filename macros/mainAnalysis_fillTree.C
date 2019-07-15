8e549f1c874bc667b03b701b4225094e073813d7
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-07-15 12:21:13 -0400

    run MCs hadEplus2

--- macros/mainAnalysis_fillTree.C
+++ macros/mainAnalysis_fillTree.C
@@ -170,8 +170,8 @@ void mainAnalysis_fillTree(const int start = 0, int end = -1, const bool doGen_ 
       tree->Add("../batch/output/mc_9306_scatElec/*.root");
    }
    else if( !doRapgap_ && doGen_){
-      tree->Add("../batch/output/mc_8926_scatElec/*.root");
-      tree->Add("../batch/output/mc_8927_scatElec/*.root");
+      tree->Add("../batch/output/mc_8926_hadEplus2/*.root");
+      tree->Add("../batch/output/mc_8927_hadEplus2/*.root");
    }
    else if( !doGen_ ){
       tree->Add("../batch/output/data_highE_06_hadEminus2/*.root");
