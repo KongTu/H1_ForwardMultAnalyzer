57e315b8333a348ff6282e59640e7f48dfc2a985
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-10-25 10:04:47 -0400

    run hadEminus 1%

--- macros/mainAnalysis_fillTree.C
+++ macros/mainAnalysis_fillTree.C
@@ -168,8 +168,8 @@ void mainAnalysis_fillTree(const int start = 0, int end = -1, const bool doGen_ 
       tree->Add("../batch/output/mc_9015/*.root");
    }
    else if( !doRapgap_ && doGen_){
-      tree->Add("../batch/output/mc_8926_elecEMinus0p5/*.root");
-      tree->Add("../batch/output/mc_8927_elecEMinus0p5/*.root");
+      tree->Add("../batch/output/mc_8926_hadEMinus0p5/*.root");
+      tree->Add("../batch/output/mc_8927_hadEMinus0p5/*.root");
    }
    else if( !doGen_ ){
       tree->Add("../batch/output/data_highE_06_hadCali/*.root");
