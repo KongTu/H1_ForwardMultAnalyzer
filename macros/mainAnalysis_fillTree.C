e4f3301339130590f9595fe07f3ae26572d10b6a
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-11-28 06:55:43 -0500

    change macros

--- macros/mainAnalysis_fillTree.C
+++ macros/mainAnalysis_fillTree.C
@@ -549,7 +549,7 @@ void mainAnalysis_fillTree(const int start = 0, int end = -1, const bool doGen_ 
             TLorentzVector eMC, gammaMC;
             eMC.SetPxPyPzE(elecPxMC,elecPyMC,elecPzMC,elecEMC);
             gammaMC.SetPxPyPzE(radPhoPxMC,radPhoPyMC,radPhoPzMC,radPhoEMC);
-            if( gammaMC.E() > 4. && eMC.E() > 4. && (gammaMC.E()+eMC.E())>20. ){
+            if( gammaMC.E() > 2. ){
                double eGammaPhiMC = eMC.DeltaPhi(gammaMC);
                myEvent.eGammaPhiMC_mini = eGammaPhiMC;
                myEvent.isQEDComptonMC_mini = 0;
