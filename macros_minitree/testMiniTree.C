bca83d1edcf6eae156db8d64606a3d3f83cc1b43
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-03-23 12:40:04 -0400

    test

--- macros_minitree/testMiniTree.C
+++ macros_minitree/testMiniTree.C
@@ -22,20 +22,25 @@ void testMiniTree(){
 
 	TTree* tree = (TTree*) file->Get("miniTree");
 	Int_t typeChgREC_mini[200];
+	Int_t passREC_mini[200];
+
 	Int_t nRECtrack_mini;
 	tree->SetBranchAddress("typeChgREC_mini",&typeChgREC_mini);
+	tree->SetBranchAddress("passREC_mini",&passREC_mini);
 	tree->SetBranchAddress("nRECtrack_mini",&nRECtrack_mini);
 
 	TH1D* h_typeChgTemp = new TH1D("h_typeChgTemp", "h_typeChgTemp ",9,-4,5);
+	TH1D* h_passREC_mini = new TH1D("h_passREC_mini", "h_passREC_mini ",9,-4,5);
 	for(int ievent = 0; ievent < tree->GetEntries(); ievent++){
 		tree->GetEntry(ievent);
 		for(int itrk = 0; itrk < nRECtrack_mini; itrk++){
-			if( typeChgREC_mini[itrk] == 0 ) cout << "typeChgREC_mini[itrk] ~ " << typeChgREC_mini[itrk] << endl;
 			h_typeChgTemp->Fill( typeChgREC_mini[itrk] );
+			h_passREC_mini->Fill( passREC_mini[itrk] );
 		}
 	}
 	
 	TFile * output = new TFile("testMiniTree.root","RECREATE");
 	h_typeChgTemp->Write();
 	output->Write();
+	h_passREC_mini->Write();
 }
