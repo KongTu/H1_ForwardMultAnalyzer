1efcc40b1a57bfad56069335f82b9e7d064a6918
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-06-15 16:00:34 -0400

    adding isDaughtersMC variable. done

--- src/ForwardMultAnalyzer.C
+++ src/ForwardMultAnalyzer.C
@@ -723,9 +723,6 @@ int main(int argc, char* argv[]) {
          myEvent.nMCtrackAll=0;
          myEvent.nMCtrack=0;
 
-         TLorentzVector K0s_Vect(0.,0.,0.,0.);
-         TLorentzVector Lam_Vect(0.,0.,0.,0.);
-
          for(int i=0;i<mcpart.GetEntries();i++) {
             
             H1PartMC *part=mcpart[i];
