f4ecf44c36f6437aa543f042e0b696414dc534fd
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-06-22 10:25:00 -0400

    check all cuts again

--- macros_minitree/readMinitree.C
+++ macros_minitree/readMinitree.C
@@ -415,10 +415,7 @@ void readMinitree(const int ifile_ = 0, const bool isReweigh = false){
 						if( sumEgamma.M() > 310. || sumEgamma.M() < 1.5 ) passcut = 0;
 						Double_t dphi=fabs(remainder(eMC.Phi()+3.1415-eGamma.Phi(),2.*3.1415)*180./3.1415);
 						if( dphi > 50. ) passcut = 0;
-
-						if( passcut == 1 ) {
-							isQEDcMC_mini = 2;
-						}
+						if(passcut == 1) continue;
 					}
 					//keep orginal QEDc events out.
 					if( isQEDcMC_mini == 2 ) continue;
