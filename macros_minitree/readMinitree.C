5d07bdcee4b5f3bb678c2e2cf938cb163116fd9a
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-06-22 11:54:35 -0400

    check all cuts again

--- macros_minitree/readMinitree.C
+++ macros_minitree/readMinitree.C
@@ -407,11 +407,11 @@ void readMinitree(const int ifile_ = 0, const bool isReweigh = false){
 						//check all cuts again:
 						int passcut = 1;
 						if( eMC.E() < 0.1 ) passcut = 0;
-						if( eMC.Theta()*TMath::RadToDeg()>178. || eMC.Theta()*TMath::RadToDeg()<3.6 ) passcut = 0;
+						if( eMC.Theta()*TMath::RadToDeg()>179. || eMC.Theta()*TMath::RadToDeg()<3.6 ) passcut = 0;
 						if( eGamma.E() < 0.1 ) passcut = 0;
-						if( eGamma.Theta()*TMath::RadToDeg()>178. || eGamma.Theta()*TMath::RadToDeg()<3.6 ) passcut = 0;
+						if( eGamma.Theta()*TMath::RadToDeg()>179. || eGamma.Theta()*TMath::RadToDeg()<3.6 ) passcut = 0;
 						if( sumEgamma.Pt() > 25. ) passcut = 0;
-						if( sumEgamma.E() < 1.5 ) passcut = 0;
+						if( sumEgamma.E() < 15. ) passcut = 0;
 						if( sumEgamma.M() > 310. || sumEgamma.M() < 1.5 ) passcut = 0;
 						Double_t dphi=fabs(remainder(eMC.Phi()+3.1415-eGamma.Phi(),2.*3.1415)*180./3.1415);
 						if( dphi > 50. ) passcut = 0;
