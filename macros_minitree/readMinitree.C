02e15f526e0d8ca939d936cec9afcbb465d5b37f
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-06-30 13:51:34 -0400

    check diff events

--- macros_minitree/readMinitree.C
+++ macros_minitree/readMinitree.C
@@ -30,7 +30,7 @@ void readMinitree(const int ifile_ = 0, const bool isReweigh = false){
 		else file = new TFile("../new_output/mc_highE_DJANGOH_fullReweight_Tree_hadCaliNewKine_addQEDbkg.root");
 	}else if(ifile_ == 2){
 		if(!isReweigh) file = new TFile("../new_output/mc_highE_RAPGAP_noReweight_Tree_hadCaliNew.root");
-		else file = new TFile("../new_output/mc_highE_RAPGAP_fullReweight_Tree_hadCaliNewKine_checkWeight.root");
+		else file = new TFile("../new_output/mc_highE_RAPGAP_fullReweight_Tree_hadCaliNewKine_check.root");
 	}
 	else if(ifile_ == 3){
 		file = new TFile("../new_output/mc_highE_PYTHIA6_noReweight_Tree_hadCaliNewKine_photoproduction.root");
@@ -44,7 +44,7 @@ void readMinitree(const int ifile_ = 0, const bool isReweigh = false){
 		else outname = "../minitree_output/Pn_hist_django_hadCaliNewKine_reweigh_addQEDbkg.root";
 	}else if( ifile_ == 2 ){
 		if(!isReweigh) outname = "../minitree_output/Pn_hist_rapgap_extendEtalabLooseTrack.root";
-		else outname = "../minitree_output/Pn_hist_rapgap_hadCaliNewKine_reweigh_checkWeight-June30.root";
+		else outname = "../minitree_output/Pn_hist_rapgap_hadCaliNewKine_reweigh_check-June30.root";
 	}
 	else if( ifile_ == 3 ){
 		outname = "../minitree_output/Pn_hist_pythia_hadCaliNewKine_photoproduction.root";
