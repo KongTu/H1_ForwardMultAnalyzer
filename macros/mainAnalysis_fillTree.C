7f554b585d309c8e072a77bc9dda35c7f4b51777
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-07-01 16:05:54 -0400

    ready to be used with final version?

--- macros/mainAnalysis_fillTree.C
+++ macros/mainAnalysis_fillTree.C
@@ -578,10 +578,10 @@ void mainAnalysis_fillTree(const int start = 0, int end = -1, const bool doGen_ 
          if( doGen_ ){
             //this is to remove overlap for pythia64 and assign PHPbkg flag
             myEvent.isPHPbkg_mini = 0;
-            // if( doRapgap_ && i>=diffractive_events && Q2MC_es>2.0 ) continue;
-            // if( doRapgap_ && i>=diffractive_events ) myEvent.isPHPbkg_mini = 1;
-            // if( !doRapgap_ && i>=dis_events && Q2MC_es>2.0 ) continue;
-            // if( !doRapgap_ && i>=dis_events ) myEvent.isPHPbkg_mini = 1;
+            if( doRapgap_ && i>=diffractive_events && Q2MC_es>2.0 ) continue;
+            if( doRapgap_ && i>=diffractive_events ) myEvent.isPHPbkg_mini = 1;
+            if( !doRapgap_ && i>=dis_events && Q2MC_es>2.0 ) continue;
+            if( !doRapgap_ && i>=dis_events ) myEvent.isPHPbkg_mini = 1;
 
             myEvent.isDIFFbkg_mini = 0;
             if(doRapgap_ && i>=dis_events 
