2999b8c86efc8a74df1084e6e04df11e6d7eae3e
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-08-15 10:00:24 -0400

    run pythia

--- macros_minitree/readMinitree.C
+++ macros_minitree/readMinitree.C
@@ -473,8 +473,6 @@ void readMinitree(const int ifile_ = 0, const bool isReweigh = false){
 		if( yREC_es_mini > ymax || yREC_es_mini < ymin ) continue;
 		if( eventpass_mini != 1 ) continue;
 		
-				cout << "Q2 " << Q2REC_es_mini << endl;
-
 		h_Q2vsX->Fill( xREC_es_mini, Q2REC_es_mini, w_mini);
 		h_y->Fill( yREC_es_mini, w_mini);
 		if( ifile_!=0 && isQEDcMC_mini==1 ) h_y_QEDc->Fill(yREC_es_mini, w_mini);
